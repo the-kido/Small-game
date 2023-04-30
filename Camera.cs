a8c886eee187213288dc8f0746c9af9e14a9e5dd
Kido <63456333+the-kido@users.noreply.github.com>
2023-04-30 10:29:36 -0400

    Camera cleanup + tweeks

--- Camera.cs
+++ Camera.cs
@@ -13,7 +13,6 @@ public partial class Camera : Camera2D
 	private const float SCALE_MAX = 1.4f, SCALE_MIN = 0.9f;
 	private float diagonalLength;
 
-
 	private MovementController movementController;
 
 	public override void _Ready() {
@@ -37,8 +36,6 @@ public partial class Camera : Camera2D
 		diagonalLength *= 0.8f;
 
 		importantObjects.Add(player);
-
-
 	}
 	public override void _Process(double delta) {
 
@@ -62,40 +59,26 @@ public partial class Camera : Camera2D
 		Vector2 topRight = importantObjectsRect.Size;
 		Vector2 diff = topRight - bottomLeft;
 
-
-		//x: 823
-		//y: 464
-		float reg = Mathf.Sqrt(823^2 + 464^2); //The diagonal's length at zoom 1.4. Can't be more than this.
-
-
 		float diagonal = Mathf.Sqrt(diff.X*diff.X + diff.Y*diff.Y);
-		//GD.Print(diagonal, " annd " , diagonalLength);
 		float dif = diagonal / diagonalLength;
 		
-        //dif *= 2f;
-
-		//GD.Print("Dif:", dif);
         dif = Mathf.Max(dif, 0.8f);
 		
 		return Vector2.One / (dif); //TODO. 
-
-		
-
 	}
 	
 	public Vector2 FinalCameraPosition(Vector2 importantObjectsRectCenter) {
 		
 		Vector2 FinalCameraPosition = Vector2.Zero;
 
+		FinalCameraPosition += importantObjectsRectCenter * 5;
 
-		FinalCameraPosition += importantObjectsRectCenter / 2;
+		FinalCameraPosition += PlayerCameraShift() * 2;
 
-		FinalCameraPosition += PlayerCameraShift() / 2;
-
-		return FinalCameraPosition;
+		return FinalCameraPosition / 7;
 	}
 
-
+	
 	private Vector2 PlayerCameraShift() {
 		Vector2 cameraShift = movementController.playerDirection * 100;
 		cameraShift.Y *= 1.5f;
@@ -122,13 +105,8 @@ public partial class Camera : Camera2D
 
 	private void OnBodyEntered(Node2D body) {
 		importantObjects.Add(body);
-		
-		// GD.Print("ENTERED SOMETHING DID, ", body.Name);
 	}
 	private void OnBodyLeave(Node2D body) {
 		importantObjects.Remove(body);
-
-		// GD.Print("LEFT SOMETHING DID, ", body.Name);
 	}
-	
 }
