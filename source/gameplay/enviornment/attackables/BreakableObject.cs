4874d3cca879bf6897614dc71c80d097d3ab6775
Kido <63456333+the-kido@users.noreply.github.com>
2023-08-18 09:28:07 -0400

    Changed timer namespace + player bug fix

--- source/gameplay/enviornment/attackables/BreakableObject.cs
+++ source/gameplay/enviornment/attackables/BreakableObject.cs
@@ -7,13 +7,11 @@ public abstract partial class BreakableObject : StaticBody2D {
     [Export]
     protected Damageable Damageable {get; private set;}
     public override void _Ready() {
-        GD.Print(Damageable);
         Damageable.OnDamaged += Flash;
         Damageable.OnDeath += Destroy;
     }
     private void Destroy(DamageInstance damageInstance) {
         // temp
-        GD.Print("should queuefree");
         QueueFree();
     }
 
