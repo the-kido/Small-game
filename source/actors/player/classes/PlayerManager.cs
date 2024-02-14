88eeda03a9413dcfd3ff08854360a077a495f985
Kido <63456333+the-kido@users.noreply.github.com>
2024-02-14 10:07:21 -0500

    Lots of cleanup

--- source/actors/player/classes/PlayerManager.cs
+++ source/actors/player/classes/PlayerManager.cs
@@ -46,17 +46,13 @@ public partial class PlayerManager : Node2D {
 }
 
 public static class PlayerClasses {
-    public static readonly Weird weird = new();
     public static readonly Normal normal = new();
     public static readonly Script NormalPlayerScript = ResourceLoader.Load<Script>("res://source/actors/player/classes/Normal.cs");
-    public static readonly Script WeirdPlayerScript = ResourceLoader.Load<Script>("res://source/actors/player/classes/Weird.cs");
 
     public static Dictionary<string, PlayerClassResource> List => new() {
         {"Normal", ResourceLoader.Load<PlayerClassResource>("res://assets/content/classes/default.tres")},
-        {"Weird", ResourceLoader.Load<PlayerClassResource>("res://assets/content/classes/weird.tres")},
     };
     public static Dictionary<string, IPlayerClass> Other => new() {
         {"Normal", normal},
-        {"Weird", weird},
     };
 }
